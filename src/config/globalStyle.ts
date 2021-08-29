22d7d76ec9bd89e0cb2b950a97e0568bd6472595
yuma watanabe <yumawatanabe@yumanoMacBook-Pro.local>
2021-08-29 16:52:25 +0900

    add-layouts-components

---
+++ src/config/globalStyle.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  button {
+    background: none;
+    border: none;
+    text-align: left;
+    cursor: pointer;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,22 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client';
 import { client } from '@/libs/apollo'
+// config
+import { GlobalStyle } from '@/config/globalStyle';
+// context
+import { StyleProvider } from '@/context/style';
+// components
+import { Layout } from '@/components/layouts';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <GlobalStyle />
+      <StyleProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </StyleProvider>
     </ApolloProvider>
   );
 }
