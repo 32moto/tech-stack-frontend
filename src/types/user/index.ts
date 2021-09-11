ca3b4d0edb6ad555cc8bda43c1e514d31df185c5
yun8boo <yuma051521@gmail.com>
2021-09-11 19:46:50 +0900

    fix

--- src/types/user/index.ts
+++ src/types/user/index.ts
@@ -1,5 +1,11 @@
 import { StackType } from "./stack";
 
+export interface CurrentUserType {
+  id: string
+  name: string
+  imagePath: string | null
+}
+
 export interface UserType {
   id: string
   name: string
